4ae0627cbfdd774836f970094d2375b57d03cf9e
Laura Voineag <laura.voineag@bbc.co.uk>
2023-06-12 13:27:33 +0100

    Testing added

--- app/src/main/java/com/example/dice/MainActivity.kt
+++ app/src/main/java/com/example/dice/MainActivity.kt
@@ -16,9 +16,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val rollButton: Button = findViewById(R.id.button)
-        rollButton.setOnClickListener {
-            rollDice()
-        }
+        rollButton.setOnClickListener { rollDice() }
 //        setContent {
 //            DiceTheme {
 //                // A surface container using the 'background' color from the theme
@@ -35,29 +33,39 @@ class MainActivity : ComponentActivity() {
     private fun rollDice() {
         val dice = Dice(6)
         val diceRoll = dice.roll()
+        val luckyNumber = 4
+
+        when (diceRoll) {
+            luckyNumber -> println("You won!")
+            1 -> println("So sorry! You rolled a 1. Try again!")
+            2 -> println("Sadly, you rolled a 2. Try again!")
+            3 -> println("Unfortunately, you rolled a 3. Try again!")
+            5 -> println("Don't cry! You rolled a 5. Try again!")
+            6 -> println("Apologies! You rolled a 6. Try again!")
+        }
         val resultTextView: TextView = findViewById(R.id.textView3)
         resultTextView.text = diceRoll.toString()
     }
 }
 
-class Dice (private val numSides:Int){
-    fun roll():Int{
+class Dice(private val numSides: Int) {
+    fun roll(): Int {
         return (1..numSides).random()
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    DiceTheme {
-        Greeting("Android")
-    }
-}
+//@Composable
+//fun Greeting(name: String, modifier: Modifier = Modifier) {
+//    Text(
+//        text = "Hello $name!",
+//        modifier = modifier
+//    )
+//}
+//
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    DiceTheme {
+//        Greeting("Android")
+//    }
+//}
--- app/src/test/java/com/example/dice/ExampleUnitTest.kt
+++ app/src/test/java/com/example/dice/ExampleUnitTest.kt
@@ -3,15 +3,16 @@ package com.example.dice
 import org.junit.Test
 
 import org.junit.Assert.*
+import org.junit.Assert.assertTrue
 
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun generates_number(){
+        //arrange
+        val dice = Dice(6)
+        //act
+        val rollResult = dice.roll()
+        //assert
+        assertTrue("The value of rollResult was not between 1 and 6",rollResult in 1..6)
     }
 }
